5d3b5194c26a574d0aade80b8b12ce256f4d2895
José Marinho <eduardo.u.marinho@gmail.com>
2023-10-08 15:40:45 +0100

    provide browser animation module to root

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './app/app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
-	providers: [importProvidersFrom(RouterModule.forRoot(appRoutes))],
+	providers: [
+		importProvidersFrom(RouterModule.forRoot(appRoutes)),
+		importProvidersFrom(BrowserAnimationsModule),
+	],
 }).catch((err) => console.error(err));
