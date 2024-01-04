c5bb1f73785eb966c0950d47448e6840f3fcb377
SirZemar <eduardo.w.marinho@gmail.com>
2024-01-04 21:39:04 +0000

    display error message if adding existing apartment

--- src/app/components/apartment-add-form/apartment-add-form.modal.component.ts
+++ src/app/components/apartment-add-form/apartment-add-form.modal.component.ts
@@ -21,12 +21,34 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { ApartmentService } from 'src/app/services/apartment/apartment.service';
-import { Subscription, finalize } from 'rxjs';
+import {
+  Observable,
+  Subject,
+  Subscription,
+  concat,
+  delay,
+  finalize,
+  of,
+  switchMap,
+} from 'rxjs';
 import { ReviewsService } from 'src/app/services/reviews/reviews.service';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-apartment-add-form',
   standalone: true,
+  animations: [
+    trigger('enterAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('200ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('200ms', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
   imports: [
     CommonModule,
     FormsModule,
@@ -44,8 +66,14 @@ export class ApartmentAddFormModalComponent implements OnDestroy {
   fb = inject(FormBuilder);
   apartmentService = inject(ApartmentService);
   reviewsService = inject(ReviewsService);
-  subscription: Subscription = new Subscription();
+  subscription = new Subscription();
+
   isLoading = signal(false);
+
+  private temporaryMessageSubject = new Subject<string>();
+  temporaryMessage$ = this.temporaryMessageSubject.pipe(
+    switchMap(value => concat(of(value), of('').pipe(delay(2000))))
+  );
 
   form = this.fb.group({
     id: new FormControl(
@@ -63,27 +91,36 @@ export class ApartmentAddFormModalComponent implements OnDestroy {
   onSubmit() {
     this.isLoading.set(true);
     const name = this.form.get('name')?.value;
+    const apartmentExist = this.apartmentService
+      .apartments()
+      .find(
+        apartment => apartment.id.toLowerCase() === this.data.id.toLowerCase()
+      );
+
+    if (apartmentExist) {
+      this.isLoading.set(false);
+      this.temporaryMessageSubject.next('Apartment already exist!');
+      return;
+    }
 
     this.subscription = this.apartmentService
       .addApartment(this.data.id, name ? { name } : { name: this.data.id })
       .pipe(
+        switchMap(() => {
+          console.log(
+            `Apartment added successfully. Now scraping reviews with id ${this.data.id}`
+          );
+          return this.reviewsService.scrapeApartmentReviews(this.data.id);
+        }),
         finalize(() => {
           this.isLoading.set(false);
           this.dialogRef.close();
         })
       )
       .subscribe({
-        next: () => {
-          console.log(
-            `Apartment added successfully. Now scraping reviews with id ${this.data.id}`
-          );
-
-          this.reviewsService.scrapeApartmentReviews(this.data.id).subscribe({
-            next: data => console.log(data),
-            error: error => console.log('Error scraping apartment', error),
-          });
-        },
-        error: error => console.log('Error adding apartment', error),
+        next: data => console.log(data),
+        error: error =>
+          console.log('Error adding or scraping apartment', error),
       });
   }
 
