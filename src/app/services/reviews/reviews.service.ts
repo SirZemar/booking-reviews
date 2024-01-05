745430aa83afd1caa0e23473b8ea2ae245927eee
SirZemar <eduardo.w.marinho@gmail.com>
2024-01-05 14:45:07 +0000

    fix: request is trying to fetch from local host

--- src/app/services/reviews/reviews.service.ts
+++ src/app/services/reviews/reviews.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, inject, OnInit } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { reviewsEndpoints } from 'src/apis/reviews';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, map, tap, throwError } from 'rxjs';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,20 +9,12 @@ import { Observable, catchError, map, tap, throwError } from 'rxjs';
 export class ReviewsService {
   http = inject(HttpClient);
 
-  getApartmentReviewRates(apartmentId: string) {
-    return this.http
-      .get(`${reviewsEndpoints.REVIEW_RATINGS}/${apartmentId}`)
-      .pipe(map(data => (data as any).reviewRates));
-  }
-
   scrapeApartmentReviews(apartmentId: string) {
-    return this.http
-      .get(`http://localhost:8080/apartments/${apartmentId}/scrapeReviews`)
-      .pipe(
-        catchError(error => {
-          console.error('Error scraping apartment:', error);
-          return throwError(() => new Error(error));
-        })
-      );
+    return this.http.get(reviewsEndpoints.scrapeReviews(apartmentId)).pipe(
+      catchError(error => {
+        console.error('Error scraping apartment:', error);
+        return throwError(() => new Error(error));
+      })
+    );
   }
 }
