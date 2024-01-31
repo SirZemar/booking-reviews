99f2c05d3d03c82ad3059ff2d4231bc04cf2273d
SirZemar <eduardo.w.marinho@gmail.com>
2024-01-31 12:17:37 +0000

    set apartment signal when apartment update

--- src/app/services/reviews/reviews.service.ts
+++ src/app/services/reviews/reviews.service.ts
@@ -1,16 +1,19 @@
 import { Injectable, inject } from '@angular/core';
 import { reviewsEndpoints } from 'src/apis/reviews';
 import { HttpClient } from '@angular/common/http';
-import { catchError, throwError } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
+import { ApartmentService } from '../apartment/apartment.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReviewsService {
   http = inject(HttpClient);
+  apartmentService = inject(ApartmentService);
 
   scrapeApartmentReviews(apartmentId: string) {
     return this.http.get(reviewsEndpoints.scrapeReviews(apartmentId)).pipe(
+      tap(() => this.apartmentService.setApartmentsSignal()),
       catchError(error => {
         console.error('Error scraping apartment:', error);
         return throwError(() => new Error(error));
