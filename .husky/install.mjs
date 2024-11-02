628dfad9068995ba99881fada2af250a8631b87b
José Marinho <eduardo.w.marinho@gmail.com>
2024-11-02 23:28:30 +0000

    fix(husky): remove undefined variable

--- .husky/install.mjs
+++ .husky/install.mjs
@@ -1,6 +1,6 @@
 // Skip husky install in production and CI
 if (process.env.NODE_ENV === 'production' || process.env.CI === 'true') {
-	process.exit(O);
+	process.exit(0);
 }
 const husky = (await import('husky')).default;
 console.log(husky());
